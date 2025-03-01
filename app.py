a49d07ab695ea32f0996a521bf67c1446a80e3f3
rick51b710 <eclipse5035@gmail.com>
2025-03-01 20:51:30 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask, render_template
 
-app = Flask(__name__, template_folder="templates")
+app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template('index.html')
 
-# 讓 Vercel 找到 `app` 變數
+# 讓 Vercel 正確辨識
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run()
