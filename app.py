8073398219fd0056c7ff484eb31ebbd09385cea7
rick51b710 <eclipse5035@gmail.com>
2025-03-01 20:38:05 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,11 @@
-{
-  "version": 2,
-  "builds": [
-    {
-      "src": "app.py",
-      "use": "@vercel/python"
-    }
-  ],
-  "routes": [
-    {
-      "src": "/(.*)",
-      "dest": "app.py"
-    }
-  ]
-}
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# 讓 Vercel 找到 app 變數
+if __name__ == '__main__':
+    app.run()
