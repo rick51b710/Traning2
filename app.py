c9efe4137ac0362cfc7cf45fbea5784115773e33
rick51b710 <eclipse5035@gmail.com>
2025-03-01 23:09:13 +0800

    更新 app.py

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return {"message": "Hello, Vercel! Your Flask app is running."}
+    return jsonify({"message": "Hello, Vercel! Your Flask app is running."})
+
+@app.route("/ping")
+def ping():
+    return jsonify({"message": "pong"}), 200
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
